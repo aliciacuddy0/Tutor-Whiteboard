8b633f8d4b0b7a86c2eab8275cdc3b930de5ddb5
aliciacuddy0 <aliciacuddy0@gmail.com>
2026-01-17 21:54:50 +0000

    Update page.tsx

--- examples/with-nextjs/src/app/page.tsx
+++ examples/with-nextjs/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Phase1Whiteboard from "../Phase1Whiteboard"; 
+import Phase1Whiteboard from "../Phase1Whiteboard";
 
 export default function Page() {
   return <Phase1Whiteboard />;
