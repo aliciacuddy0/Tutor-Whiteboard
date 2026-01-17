9f0159c7973e50b71111aca24ac489045b9cf4d2
aliciacuddy0 <aliciacuddy0@gmail.com>
2026-01-17 21:13:43 +0000

    Update page.tsx

--- examples/with-nextjs/src/app/page.tsx
+++ examples/with-nextjs/src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Phase1Whiteboard from "../Phase1Whiteboard";
 
-export default function Home() {
-  return <Phase1Whiteboard />;
+export default function Page() {
+  return (
+    <Suspense fallback={null}>
+      <Phase1Whiteboard />
+    </Suspense>
+  );
 }
